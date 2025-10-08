40713be178671692067739b05826c3dcc5ff9605
kirandeepak5355 <88786817+kirandeepak5355@users.noreply.github.com>
2025-10-08 16:31:16 +0530

    Update Program.cs

--- LibraryAPI/Program.cs
+++ LibraryAPI/Program.cs
@@ -83,4 +83,6 @@ app.UseAuthentication(); // JWT Authentication
 app.UseAuthorization();
 
 app.MapControllers();
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
+
